ebf51689e9aed927a77fc0bbe72f9cc23edc5578
luciodanielcv <lcastelan@gmail.com>
2020-05-02 17:10:07 -0500

    adding another class that accepts variables in the url

--- src/main/java/com/course/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/course/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.course.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +19,15 @@ public class HelloWorldController {
 		return "Hello World!";
 	}
 	
-	@GetMapping(path="hello-world-bean")
+	@GetMapping(path="/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World Bean!");
+	}
+	
+	@GetMapping(path="/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldBean( @PathVariable String name ) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+		
 	}
 	
 }
